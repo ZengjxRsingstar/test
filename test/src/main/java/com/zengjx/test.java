bb2e08fe027a0a00a1e15060bd487cb224e05abb
zengjx <1164512033@qq.com>
2019-10-26 22:07:09 +0800

    2019-10-26 第一次提交

---
+++ test/src/main/java/com/zengjx/test.java
@@ -0,0 +1,15 @@
+package com.zengjx;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author zengjx
+ * @Company zengjx
+ * @Date 2019/10/26  21:57
+ * @Version V1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+    }
+}
